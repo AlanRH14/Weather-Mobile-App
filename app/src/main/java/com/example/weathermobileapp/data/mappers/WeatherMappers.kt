0a2a83b071d499cc047f4d8a40256e4dafb6ce03
Alan Andres Rosas Hernandez <alan.rosash@elektra.com.mx>
2024-09-27 14:00:16 -0600

    Mapper data HourlyWeatherItem.

--- app/src/main/java/com/example/weathermobileapp/data/mappers/WeatherMappers.kt
+++ app/src/main/java/com/example/weathermobileapp/data/mappers/WeatherMappers.kt
@@ -1,7 +1,9 @@
 package com.example.weathermobileapp.data.mappers
 
+import com.example.weathermobileapp.R
 import com.example.weathermobileapp.data.local.mockdata.WeatherMockData.WeatherData
 import com.example.weathermobileapp.data.remote.dto.WeatherDto
+import com.example.weathermobileapp.domain.models.WeatherDataModel
 import com.example.weathermobileapp.domain.models.WeatherLocationModel
 import com.example.weathermobileapp.domain.models.WeatherModel
 import com.example.weathermobileapp.domain.models.WeatherType
@@ -15,7 +17,23 @@ fun WeatherDto.toWeatherModel(): WeatherModel {
             location = "${this.name}",
             description = "${this.weather?.first()?.description}"
         ),
-        weatherData = WeatherData.weatherData,
+        weatherData = listOf(
+            WeatherDataModel(
+                iconRes = R.drawable.ic_pressure,
+                title = "Pressure",
+                subtitle = "${this.main?.pressure ?: 0}hpa"
+            ),
+            WeatherDataModel(
+                iconRes = R.drawable.ic_wind,
+                title = "Wind",
+                subtitle = "${Math.round(this.wind?.speed ?: 0.0)}Km/s"
+            ),
+            WeatherDataModel(
+                iconRes = R.drawable.ic_drop,
+                title = "Humidity",
+                subtitle = "${this.main?.humidity ?: 0}%"
+            )
+        ),
         hourlyWeathers = WeatherData.hourlyWeathers,
         dailyWeathers = WeatherData.dailyWeathers,
     )
