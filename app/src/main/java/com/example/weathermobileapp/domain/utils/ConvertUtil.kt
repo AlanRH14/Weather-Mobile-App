0382ed158b885656cad1aa1077308176289ac1f7
Alan Andres Rosas Hernandez <alan.rosash@elektra.com.mx>
2024-09-30 17:03:30 -0600

    Mapper data HourlyWeatherModel. Date logic for HourlyWeatherModel and components.

--- app/src/main/java/com/example/weathermobileapp/domain/utils/ConvertUtil.kt
+++ app/src/main/java/com/example/weathermobileapp/domain/utils/ConvertUtil.kt
@@ -1,10 +1,11 @@
 package com.example.weathermobileapp.domain.utils
 
+import android.util.Log
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-fun Int?.toDateFormat(): String {
+fun Int?.toCustomDateFormat(): String {
     return try {
         if (this != null) {
             val dateFormat = SimpleDateFormat("EEE MMM d | h:mm a", Locale.US)
@@ -13,7 +14,47 @@ fun Int?.toDateFormat(): String {
         } else {
             "N/A"
         }
-    }catch (e: Exception){
+    } catch (e: Exception) {
         "N/A"
     }
+}
+
+fun Int?.toHourFormat(): String {
+    return try {
+        if (this != null) {
+            val dateFormat = SimpleDateFormat("dd h:mm a", Locale.US)
+            val date = Date(this * 1000L)
+            dateFormat.format(date)
+        } else {
+            "N/A"
+        }
+    } catch (e: Exception) {
+        "N/A"
+    }
+}
+
+fun compareDate() {
+    val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.US)
+    val currentDay = dateFormat.format(System.currentTimeMillis())
+    val date = Date(1727978400 * 1000L)
+    val weatherDate = dateFormat.format(date)
+    val tomorrow = currentDay.plus(1)
+    when {
+        weatherDate == currentDay -> {
+            Log.d("LordMiau", "Res: Hoy $currentDay")
+        }
+
+        weatherDate == tomorrow -> {
+            Log.d("LordMiau", "Res: Mañana $currentDay")
+        }
+
+        weatherDate > tomorrow -> {
+            Log.d("LordMiau", "Res: De más días $currentDay")
+        }
+
+        else -> {
+            Log.d("LordMiau", "Res: N/A")
+        }
+    }
+
 }
