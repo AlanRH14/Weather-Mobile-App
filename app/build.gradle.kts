6a98e7517748e065dc7d011d4732286481096864
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-24 13:29:33 -0600

    Update libraries versions part. 16.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.example.weathermobileapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.weathermobileapp"
