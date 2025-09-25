e6bc3b6b0edbdfc289725313092b09ad7eea5652
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-25 11:21:40 -0600

    Config koin library part. 6.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,6 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.icons.extended)
 
-    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -65,6 +64,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.play.services.location)
 
     ksp(libs.hilt.android.compiler)
     implementation(libs.hilt.androidx.navigation.compose)
