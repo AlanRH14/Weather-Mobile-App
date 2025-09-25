363ab8b9b8c23445b51e5d2f546a8ed423a1a2f2
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-25 13:01:13 -0600

    Config koin module part. 29.

--- app/src/main/java/com/example/weathermobileapp/di/LocationModule.kt
+++ app/src/main/java/com/example/weathermobileapp/di/LocationModule.kt
@@ -5,9 +5,6 @@ import com.example.weathermobileapp.domain.location.LocationTracker
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import javax.inject.Singleton
 
 abstract class LocationModule {
 
