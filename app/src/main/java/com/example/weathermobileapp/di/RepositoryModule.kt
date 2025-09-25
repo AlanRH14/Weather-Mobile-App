e89b9709c409af2cdc9c880f5e534779e1a76447
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-25 13:38:29 -0600

    Config koin module part. 43.

--- app/src/main/java/com/example/weathermobileapp/di/RepositoryModule.kt
+++ app/src/main/java/com/example/weathermobileapp/di/RepositoryModule.kt
@@ -2,9 +2,6 @@ package com.example.weathermobileapp.di
 
 import com.example.weathermobileapp.data.repository.WeatherRepositoryImpl
 import com.example.weathermobileapp.domain.repository.WeatherRepository
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
 
 abstract class RepositoryModule {
     abstract fun bindWeatherRepository(
