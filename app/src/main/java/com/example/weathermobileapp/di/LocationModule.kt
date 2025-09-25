e6bff0187061e8eb5471244d6edfe5bae3174ab4
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-25 13:05:05 -0600

    Config koin module part. 33.

--- app/src/main/java/com/example/weathermobileapp/di/LocationModule.kt
+++ app/src/main/java/com/example/weathermobileapp/di/LocationModule.kt
@@ -7,9 +7,6 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val locationModule = module {
-
-    abstract fun bindLocationTracker(defaultLocationTracker: LocationTrackerImpl): LocationTracker
-
     single<LocationTracker> {
         LocationTrackerImpl(
             locationClient = get(),
