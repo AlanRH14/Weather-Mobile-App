64f6d851b6867a6bfd1dd047b54bb180e0f6b6d8
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-25 16:37:01 -0600

    Config koin module part. 73.

--- app/src/main/java/com/example/weathermobileapp/presentation/screens/weather/WeatherViewModel.kt
+++ app/src/main/java/com/example/weathermobileapp/presentation/screens/weather/WeatherViewModel.kt
@@ -5,13 +5,11 @@ import androidx.lifecycle.viewModelScope
 import com.example.weathermobileapp.domain.ResultApi
 import com.example.weathermobileapp.domain.location.LocationTracker
 import com.example.weathermobileapp.domain.repository.WeatherRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-@HiltViewModel
 class WeatherViewModel(
     private val repository: WeatherRepository,
     private val locationTracker: LocationTracker
