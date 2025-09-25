ec4e2f4de40646bf8e2ca2ed96820a49dc926289
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-25 12:50:34 -0600

    Config koin module part. 26.

--- app/src/main/java/com/example/weathermobileapp/di/AppModule.kt
+++ app/src/main/java/com/example/weathermobileapp/di/AppModule.kt
@@ -1,8 +1,6 @@
 package com.example.weathermobileapp.di
 
 import android.app.Application
-import com.example.weathermobileapp.data.remote.api.ApiConfig.BASE_URL
-import com.example.weathermobileapp.data.remote.api.WeatherApi
 import com.example.weathermobileapp.presentation.screens.next_days_forecast.ForecastViewModel
 import com.example.weathermobileapp.presentation.screens.weather.WeatherViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
