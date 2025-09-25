a3a2c57e8cef1c05fa82563e0e49224f20a6d6f7
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-25 13:32:37 -0600

    Config koin module part. 40.

--- app/src/main/java/com/example/weathermobileapp/di/RepositoryModule.kt
+++ app/src/main/java/com/example/weathermobileapp/di/RepositoryModule.kt
@@ -8,8 +8,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
 
     @Binds
