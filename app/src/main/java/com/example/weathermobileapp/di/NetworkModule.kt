00cbae33243c999e2608dbf0ece2c634976670a5
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-25 12:24:55 -0600

    Config koin module part. 14.

--- app/src/main/java/com/example/weathermobileapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/weathermobileapp/di/NetworkModule.kt
@@ -1,7 +1,20 @@
 package com.example.weathermobileapp.di
 
+import com.example.weathermobileapp.data.remote.api.ApiConfig.BASE_URL
 import org.koin.dsl.module
+import retrofit2.Retrofit
 
 val networkModule = module {
+    single {
+        HttpLoggingInterceptor().apply {
 
+        }
+    }
+    single {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client()
+            .addConverterFactory()
+            .build()
+    }
 }
