ff4dee6ce96905d2fa8ae0fb05aa82aa1ee2b7c0
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-25 13:05:19 -0600

    Config koin module part. 34.

--- app/src/main/java/com/example/weathermobileapp/di/LocationModule.kt
+++ app/src/main/java/com/example/weathermobileapp/di/LocationModule.kt
@@ -3,7 +3,6 @@ package com.example.weathermobileapp.di
 import com.example.weathermobileapp.data.location.LocationTrackerImpl
 import com.example.weathermobileapp.domain.location.LocationTracker
 import org.koin.android.ext.koin.androidApplication
-import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val locationModule = module {
