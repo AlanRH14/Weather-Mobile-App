be6a0edd64ff7554ed577d15d7c08f4769517c70
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-25 12:43:18 -0600

    Config koin module part. 21.

--- app/src/main/java/com/example/weathermobileapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/weathermobileapp/di/NetworkModule.kt
@@ -7,6 +7,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 
 private val json = Json {
     coerceInputValues = true
@@ -32,7 +33,7 @@ val networkModule = module {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(get<OkHttpClient>())
-            .addConverterFactory()
+            .addConverterFactory(json.asConverterFactory(contentType))
             .build()
     }
 }
