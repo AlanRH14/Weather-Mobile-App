de59ddd3b1eb1588518f8f914260c8322c14b2d5
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-25 16:34:54 -0600

    Config koin module part. 71.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    implementation(libs.androidx.navigation.compose)
+
     implementation(libs.play.services.location)
 
     implementation(libs.koin.android)
