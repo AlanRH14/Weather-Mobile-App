b2683b4c12996acc6b66496c6820eb817f39aa64
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-25 13:45:25 -0600

    Config koin module part. 53.

--- app/src/main/java/com/example/weathermobileapp/WeatherApplication.kt
+++ app/src/main/java/com/example/weathermobileapp/WeatherApplication.kt
@@ -5,14 +5,16 @@ import com.example.weathermobileapp.di.appModule
 import com.example.weathermobileapp.di.locationModule
 import com.example.weathermobileapp.di.networkModule
 import com.example.weathermobileapp.di.repositoryModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class WeatherApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
         startKoin {
+            androidContext(this@WeatherApplication)
+
             modules(
                 networkModule,
                 repositoryModule,
