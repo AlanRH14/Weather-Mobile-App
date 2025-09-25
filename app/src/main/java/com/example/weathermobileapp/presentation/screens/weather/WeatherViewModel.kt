8fbbb5ceaa7da1685ffe85cb1ce15671fa3e1924
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-25 16:19:53 -0600

    Config koin module part. 61.

--- app/src/main/java/com/example/weathermobileapp/presentation/screens/weather/WeatherViewModel.kt
+++ app/src/main/java/com/example/weathermobileapp/presentation/screens/weather/WeatherViewModel.kt
@@ -10,10 +10,9 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @HiltViewModel
-class WeatherViewModel @Inject constructor(
+class WeatherViewModel(
     private val repository: WeatherRepository,
     private val locationTracker: LocationTracker
 ) : ViewModel() {
