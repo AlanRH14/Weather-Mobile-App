ba1c62cf669b9213808daa9db94730427dbc64e3
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-29 11:21:56 -0600

    Config MVI module part. 21.

--- app/src/main/java/com/example/weathermobileapp/MainActivity.kt
+++ app/src/main/java/com/example/weathermobileapp/MainActivity.kt
@@ -17,19 +17,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        /*permissionLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) {
-            viewModel.getWeatherData()
-            viewModel.getWeatherForecastData()
-        }
         permissionLauncher.launch(
             arrayOf(
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION,
             )
-        )*/
+        )
 
         setContent {
             WeatherMobileAppTheme {
