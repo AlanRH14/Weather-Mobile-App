fad386fb2f21d3f987210712bfa0a3e3ce192cc8
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-29 09:38:21 -0600

    Config MVI module part. 3.

--- app/src/main/java/com/example/weathermobileapp/presentation/screens/weather/WeatherUIEvent.kt
+++ app/src/main/java/com/example/weathermobileapp/presentation/screens/weather/WeatherUIEvent.kt
@@ -1,5 +1,5 @@
 package com.example.weathermobileapp.presentation.screens.weather
 
 sealed interface WeatherUIEvent {
-    data object OnGeWeather : WeatherUIEvent
+    data object OnGetWeather : WeatherUIEvent
 }
