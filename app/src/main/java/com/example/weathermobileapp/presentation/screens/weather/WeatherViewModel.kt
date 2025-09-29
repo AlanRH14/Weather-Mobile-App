28787dfbb0cd064343eb60c07e1028b40643b82d
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-29 09:43:33 -0600

    Config MVI module part. 9.

--- app/src/main/java/com/example/weathermobileapp/presentation/screens/weather/WeatherViewModel.kt
+++ app/src/main/java/com/example/weathermobileapp/presentation/screens/weather/WeatherViewModel.kt
@@ -24,8 +24,9 @@ class WeatherViewModel(
     val effect = _effect.asSharedFlow()
 
     fun onEvent(event: WeatherUIEvent) {
-        when(event) {
+        when (event) {
             is WeatherUIEvent.OnGetWeather -> getWeatherData()
+            is WeatherUIEvent.OnGetWeatherForecast -> getWeatherForecastData()
         }
     }
 
