67d783873d321af66a59b905fbff4ecafcd3ad98
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-29 11:05:53 -0600

    Config MVI module part. 12.

--- app/src/main/java/com/example/weathermobileapp/MainActivity.kt
+++ app/src/main/java/com/example/weathermobileapp/MainActivity.kt
@@ -5,16 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.example.weathermobileapp.navigation.AppNavHost
-import com.example.weathermobileapp.presentation.screens.weather.WeatherViewModel
 import com.example.weathermobileapp.ui.theme.WeatherMobileAppTheme
-import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
