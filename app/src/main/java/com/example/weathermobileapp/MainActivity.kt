9aa61820b6eb36aac6e6238ee319f193278b3feb
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-29 11:25:27 -0600

    Config MVI module part. 23.

--- app/src/main/java/com/example/weathermobileapp/MainActivity.kt
+++ app/src/main/java/com/example/weathermobileapp/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.weathermobileapp
 
-import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
