d62190f0d26687c30d4fa618504ff4c3a63e922a
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-29 13:50:33 -0600

    Config MVI module part. 64.

--- app/src/main/java/com/example/weathermobileapp/presentation/screens/next_days_forecast/NextDaysForecastUIEvent.kt
+++ app/src/main/java/com/example/weathermobileapp/presentation/screens/next_days_forecast/NextDaysForecastUIEvent.kt
@@ -2,4 +2,5 @@ package com.example.weathermobileapp.presentation.screens.next_days_forecast
 
 sealed interface NextDaysForecastUIEvent {
     data object OnGetWeatherForecastData : NextDaysForecastUIEvent
+    data class OnDayOfWeekUpdate(val dayOfWeek: String) : NextDaysForecastUIEvent
 }
