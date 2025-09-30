7dcb06a006bcdc6aa6cbe564a5d559685f54ccbd
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-30 13:28:09 -0600

    Change api mapper to factory pattern part. 17

---
+++ app/src/main/java/com/example/weathermobileapp/di/ApiMapperModule.kt
@@ -0,0 +1,15 @@
+package com.example.weathermobileapp.di
+
+import com.example.weathermobileapp.common.ApiMapper
+import com.example.weathermobileapp.data.mappers_impl.DailyWeatherMapperImpl
+import com.example.weathermobileapp.data.remote.dto.forecast.Forecast
+import com.example.weathermobileapp.domain.models.DailyWeatherModel
+import org.koin.dsl.module
+
+val apiMapperModule = module {
+    single<ApiMapper<List<Forecast?>?, List<DailyWeatherModel>>> {
+        DailyWeatherMapperImpl()
+    }
+
+
+}
