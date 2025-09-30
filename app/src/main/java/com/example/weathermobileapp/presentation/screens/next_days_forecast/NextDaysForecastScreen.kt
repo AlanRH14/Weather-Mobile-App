eb35aff1aa0ec08b2749f45fea8504b3977eab9f
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-30 09:34:27 -0600

    Config MVI module part. 73.

--- app/src/main/java/com/example/weathermobileapp/presentation/screens/next_days_forecast/NextDaysForecastScreen.kt
+++ app/src/main/java/com/example/weathermobileapp/presentation/screens/next_days_forecast/NextDaysForecastScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
 import com.example.weathermobileapp.data.local.mockdata.WeatherMockData.ErrorMock
 import com.example.weathermobileapp.data.mappers.DayOfWeekMapper.getDayOfWeek
 import com.example.weathermobileapp.presentation.components.items.DailyWeatherItem
@@ -29,6 +30,7 @@ import org.koin.androidx.compose.koinViewModel
 fun NextDaysForecastScreen(
     modifier: Modifier = Modifier,
     forecastVM: ForecastViewModel = koinViewModel(),
+    navController: NavHostController
 ) {
     var dayOfWeek by remember { mutableStateOf("") }
     val state by forecastVM.state.collectAsStateWithLifecycle()
