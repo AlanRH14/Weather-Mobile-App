ff22935617a4327dd9643ded85cceb350b522c8c
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-30 10:14:30 -0600

    Config MVI module part. 77.

--- app/src/main/java/com/example/weathermobileapp/presentation/screens/next_days_forecast/NextDaysForecastScreen.kt
+++ app/src/main/java/com/example/weathermobileapp/presentation/screens/next_days_forecast/NextDaysForecastScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -56,8 +58,8 @@ fun NextDaysForecastScreen(
             modifier = modifier
                 .background(BackGroundColor)
                 .fillMaxSize()
-                .padding(ScreenPadding),
-            c
+                .padding(ScreenPadding)
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             nextDayWeather.forEach { forecast ->
