eacb42963bf0347fddb5d40663dbb0e8f893b569
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-30 12:24:31 -0600

    Change api mapper to factory pattern part. 3

--- app/src/main/java/com/example/weathermobileapp/data/mappers_impl/DailyWeatherMappers.kt
+++ app/src/main/java/com/example/weathermobileapp/data/mappers_impl/DailyWeatherMappers.kt
@@ -1,5 +1,6 @@
 package com.example.weathermobileapp.data.mappers_impl
 
+import com.example.weathermobileapp.common.ApiMapper
 import com.example.weathermobileapp.data.remote.dto.forecast.Forecast
 import com.example.weathermobileapp.domain.models.DailyWeatherModel
 import com.example.weathermobileapp.domain.models.WeatherType
@@ -7,16 +8,19 @@ import com.example.weathermobileapp.domain.utils.DatePatternKeys.WEEKDAY
 import com.example.weathermobileapp.domain.utils.DateUtils.toCustomDateFormat
 import kotlin.math.roundToInt
 
-fun List<Forecast?>?.toDailyWeathers(): List<DailyWeatherModel> {
-    return this?.filterNotNull()
-        ?.mapNotNull { forecast ->
-            forecast.weather?.firstOrNull()?.let { weather ->
-                DailyWeatherModel(
-                    day = forecast.dt.toCustomDateFormat(WEEKDAY),
-                    image = WeatherType.fromId(weather.id).icon,
-                    lowTemp = "${(forecast.main?.tempMin ?: 0.0).roundToInt()}°",
-                    highTemp = "${(forecast.main?.tempMax ?: 0.0).roundToInt()}°"
-                )
-            }
-        } ?: emptyList()
+class DailyWeatherMapperImpl : ApiMapper<List<Forecast?>?, List<DailyWeatherModel>> {
+
+    override fun mapToDomain(dto: List<Forecast?>?): List<DailyWeatherModel> {
+        return dto?.filterNotNull()
+            ?.mapNotNull { forecast ->
+                forecast.weather?.firstOrNull()?.let { weather ->
+                    DailyWeatherModel(
+                        day = forecast.dt.toCustomDateFormat(pattern = WEEKDAY),
+                        image = WeatherType.fromId(code = weather.id).icon,
+                        lowTemp = "${(forecast.main?.tempMin ?: 0.0).roundToInt()}",
+                        highTemp = ""
+                    )
+                }
+            } ?: emptyList()
+    }
 }
