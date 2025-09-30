0a3e8bef1978f6b8db4d7672f1a6b25e16b3cc93
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-30 10:27:54 -0600

    Config MVI module part. 81.

--- app/src/main/java/com/example/weathermobileapp/presentation/screens/next_days_forecast/NextDaysForecastScreen.kt
+++ app/src/main/java/com/example/weathermobileapp/presentation/screens/next_days_forecast/NextDaysForecastScreen.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -68,9 +72,12 @@ fun NextDaysForecastScreen(
                 title = { },
                 navigationIcon = {
                     IconButton(
-                        onClick = { forecastVM.onEvent(event = NextDaysForecastUIEvent.NavigateToBack) }
+                        onClick = { forecastVM.onEvent(event = NextDaysForecastUIEvent.OnNavigateToBack) }
                     ) {
-
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = ""
+                        )
                     }
                 }
             )
