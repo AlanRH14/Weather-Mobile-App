7108680792a371da5efa473a8faea4f96e8f338d
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-30 13:00:17 -0600

    Change api mapper to factory pattern part. 15

--- app/src/main/java/com/example/weathermobileapp/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/weathermobileapp/data/repository/WeatherRepositoryImpl.kt
@@ -6,7 +6,9 @@ import com.example.weathermobileapp.data.mappers_impl.toWeatherModel
 import com.example.weathermobileapp.data.remote.api.ApiConfig.API_KEY
 import com.example.weathermobileapp.data.remote.api.WeatherApi
 import com.example.weathermobileapp.data.remote.dto.current.WeatherDto
+import com.example.weathermobileapp.data.remote.dto.forecast.Forecast
 import com.example.weathermobileapp.domain.ResultApi
+import com.example.weathermobileapp.domain.models.HourlyWeatherModel
 import com.example.weathermobileapp.domain.models.WeatherForecast
 import com.example.weathermobileapp.domain.models.WeatherModel
 import com.example.weathermobileapp.domain.repository.WeatherRepository
@@ -15,7 +17,8 @@ import kotlinx.coroutines.flow.flow
 
 class WeatherRepositoryImpl(
     private val api: WeatherApi,
-    private val apiWeatherMapper: ApiMapper<WeatherDto, WeatherModel>
+    private val apiWeatherMapper: ApiMapper<WeatherDto, WeatherModel>,
+    private val apiForecastMapper: ApiMapper<List<Forecast?>?, WeatherForecast>
 ) : WeatherRepository {
 
     override fun getWeatherData(lat: Double, lon: Double): Flow<ResultApi<WeatherModel>> =
@@ -39,7 +42,7 @@ class WeatherRepositoryImpl(
             try {
                 val weatherForecastData =
                     api.getWeatherForecastData(lat = lat, lon = lon, id = API_KEY)
-                emit(ResultApi.Success(weatherForecastData.forecasts.toForecastWeathers()))
+                emit(ResultApi.Success(apiForecastMapper.mapToDomain(dto = weatherForecastData.forecasts)))
             } catch (e: Exception) {
                 emit(ResultApi.Error(message = "Error: ${e.message}"))
             }
