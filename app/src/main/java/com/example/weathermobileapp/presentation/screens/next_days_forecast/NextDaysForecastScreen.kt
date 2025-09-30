e3a9024750f3b6372e490c608658e427044e2520
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-30 10:14:43 -0600

    Config MVI module part. 78.

--- app/src/main/java/com/example/weathermobileapp/presentation/screens/next_days_forecast/NextDaysForecastScreen.kt
+++ app/src/main/java/com/example/weathermobileapp/presentation/screens/next_days_forecast/NextDaysForecastScreen.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
