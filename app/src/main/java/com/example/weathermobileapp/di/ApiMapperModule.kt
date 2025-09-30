13466179d887f48b92c9c12ff8fa8a9cfed83c0a
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-30 13:36:21 -0600

    Change api mapper to factory pattern part. 19

--- app/src/main/java/com/example/weathermobileapp/di/ApiMapperModule.kt
+++ app/src/main/java/com/example/weathermobileapp/di/ApiMapperModule.kt
@@ -3,8 +3,10 @@ package com.example.weathermobileapp.di
 import com.example.weathermobileapp.common.ApiMapper
 import com.example.weathermobileapp.data.mappers_impl.DailyWeatherMapperImpl
 import com.example.weathermobileapp.data.mappers_impl.ForecastMapperImpl
+import com.example.weathermobileapp.data.mappers_impl.HourlyWeatherMapperImpl
 import com.example.weathermobileapp.data.remote.dto.forecast.Forecast
 import com.example.weathermobileapp.domain.models.DailyWeatherModel
+import com.example.weathermobileapp.domain.models.HourlyWeatherModel
 import com.example.weathermobileapp.domain.models.WeatherForecast
 import org.koin.dsl.module
 
@@ -18,5 +20,9 @@ val apiMapperModule = module {
             apiDailyWeatherMapper = get(),
             apiHourlyWeathersMapper = get()
         )
+    }
+
+    single<ApiMapper<List<Forecast?>?, List<HourlyWeatherModel>>> {
+        HourlyWeatherMapperImpl()
     }
 }
