0be655558a5a37d8e6560fc3f38a38ec0a668a7f
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-30 12:33:15 -0600

    Change api mapper to factory pattern part. 5

--- app/src/main/java/com/example/weathermobileapp/data/mappers_impl/ForecastMappers.kt
+++ app/src/main/java/com/example/weathermobileapp/data/mappers_impl/ForecastMappers.kt
@@ -2,20 +2,22 @@ package com.example.weathermobileapp.data.mappers_impl
 
 import com.example.weathermobileapp.common.ApiMapper
 import com.example.weathermobileapp.data.remote.dto.forecast.Forecast
+import com.example.weathermobileapp.domain.models.DailyWeatherModel
 import com.example.weathermobileapp.domain.models.WeatherForecast
 import com.example.weathermobileapp.domain.utils.DateUtils.isNextDays
 import com.example.weathermobileapp.domain.utils.DateUtils.isToday
 import com.example.weathermobileapp.domain.utils.DateUtils.isTomorrow
 
-class ForecastMapperImpl(): ApiMapper<List<Forecast?>?, WeatherForecast> {
+class ForecastMapperImpl(
+    private val apiDailyWeatherMapper: ApiMapper<List<Forecast?>?, List<DailyWeatherModel>>
+) : ApiMapper<List<Forecast?>?, WeatherForecast> {
 
     override fun mapToDomain(dto: List<Forecast?>?): WeatherForecast {
         return WeatherForecast(
-                todayWeather = dto?.filter { forecast -> forecast?.dt.isToday() }.toHourlyWeathers(),
-                tomorrowWeather = forecast.dt.isTomorrow(),
-                nextDayWeather = forecast.dt.isNextDays()
-            )
-        }
+            todayWeather = dto?.filter { forecast -> forecast?.dt.isToday() }.toHourlyWeathers(),
+            tomorrowWeather = apiDailyWeatherMapper.mapToDomain(dto = dto?.filter { forecast -> forecast?.dt.isTomorrow() }),
+            nextDayWeather = forecast.dt.isNextDays()
+        )
     }
 }
 fun List<Forecast?>?.toForecastWeathers(): WeatherForecast {
