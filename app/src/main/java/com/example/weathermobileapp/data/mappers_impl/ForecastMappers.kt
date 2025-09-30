4eacacdbcabfbc44536ca78c36a45bf4ad79d8a6
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-30 12:29:48 -0600

    Change api mapper to factory pattern part. 4

--- app/src/main/java/com/example/weathermobileapp/data/mappers_impl/ForecastMappers.kt
+++ app/src/main/java/com/example/weathermobileapp/data/mappers_impl/ForecastMappers.kt
@@ -1,11 +1,23 @@
 package com.example.weathermobileapp.data.mappers_impl
 
+import com.example.weathermobileapp.common.ApiMapper
 import com.example.weathermobileapp.data.remote.dto.forecast.Forecast
 import com.example.weathermobileapp.domain.models.WeatherForecast
 import com.example.weathermobileapp.domain.utils.DateUtils.isNextDays
 import com.example.weathermobileapp.domain.utils.DateUtils.isToday
 import com.example.weathermobileapp.domain.utils.DateUtils.isTomorrow
 
+class ForecastMapperImpl(): ApiMapper<List<Forecast?>?, WeatherForecast> {
+
+    override fun mapToDomain(dto: List<Forecast?>?): WeatherForecast {
+        return WeatherForecast(
+                todayWeather = dto?.filter { forecast -> forecast?.dt.isToday() }.toHourlyWeathers(),
+                tomorrowWeather = forecast.dt.isTomorrow(),
+                nextDayWeather = forecast.dt.isNextDays()
+            )
+        }
+    }
+}
 fun List<Forecast?>?.toForecastWeathers(): WeatherForecast {
     return WeatherForecast(
         todayWeather = this?.filter { forecast -> forecast?.dt.isToday() }
