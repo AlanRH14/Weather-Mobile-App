b75e018e36a2d3d9c8ba8ab644ae260fcec81e4b
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-30 12:49:33 -0600

    Change api mapper to factory pattern part. 8

--- app/src/main/java/com/example/weathermobileapp/data/mappers_impl/HourlyWeatherMappers.kt
+++ app/src/main/java/com/example/weathermobileapp/data/mappers_impl/HourlyWeatherMappers.kt
@@ -1,5 +1,6 @@
 package com.example.weathermobileapp.data.mappers_impl
 
+import com.example.weathermobileapp.common.ApiMapper
 import com.example.weathermobileapp.data.remote.dto.forecast.Forecast
 import com.example.weathermobileapp.domain.models.HourlyWeatherModel
 import com.example.weathermobileapp.domain.models.WeatherType
@@ -7,15 +8,18 @@ import com.example.weathermobileapp.domain.utils.DatePatternKeys.HOUR
 import com.example.weathermobileapp.domain.utils.DateUtils.toCustomDateFormat
 import kotlin.math.roundToInt
 
-fun List<Forecast?>?.toHourlyWeathers(): List<HourlyWeatherModel> {
-    return this?.filterNotNull()
-        ?.mapNotNull { forecast ->
-            forecast.weather?.firstOrNull()?.let { weather ->
-                HourlyWeatherModel(
-                    image = WeatherType.fromId(weather.id).icon,
-                    hour = forecast.dt.toCustomDateFormat(HOUR),
-                    degrees = "${(forecast.main?.temp ?: 0.0).roundToInt()}°"
-                )
-            }
-        } ?: emptyList()
+class HourlyWeatherMapperImpl : ApiMapper<List<Forecast?>?, List<HourlyWeatherModel>> {
+
+    override fun mapToDomain(dto: List<Forecast?>?): List<HourlyWeatherModel> {
+        return dto?.filterNotNull()
+            ?.mapNotNull { forecast ->
+                forecast.weather?.firstOrNull()?.let { weather ->
+                    HourlyWeatherModel(
+                        image = WeatherType.fromId(weather.id).icon,
+                        hour = forecast.dt.toCustomDateFormat(pattern = HOUR),
+                        degrees = "${(forecast.main?.temp ?: 0.0).roundToInt()}"
+                    )
+                }
+            } ?: emptyList()
+    }
 }
